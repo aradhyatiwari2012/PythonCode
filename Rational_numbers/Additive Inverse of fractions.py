54c6ceaf9bd2d3410378442e2f9d294fa5a223f0
aradhyatiwari2012 <babali.ramesh@gmail.com>
2025-03-14 22:42:31 +0530

    imported common for fraction function

--- Rational_numbers/Additive Inverse of fractions.py
+++ Rational_numbers/Additive Inverse of fractions.py
@@ -1,11 +1,8 @@
-def fraction(variable1):
-    fraction=variable1.split("/")
-    return(fraction)
-
+import common
 numb_1=input("Write the fraction:")
 
-num_a=fraction(numb_1)[0]
-den_b=fraction(numb_1)[1]
+num_a=common.fraction(numb_1)[0]
+den_b=common.fraction(numb_1)[1]
 
 num_1=int(num_a)*-1
 
