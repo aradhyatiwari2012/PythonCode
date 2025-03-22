70b883b1b36a0fa84f252ae1de763da2cad4dc7f
aradhyatiwari2012 <babali.ramesh@gmail.com>
2025-03-22 11:50:02 +0530

    New file added

---
+++ Linear Eq, Shapes/Turtle_draw_rect.py
@@ -0,0 +1,17 @@
+import turtle  
+def draw_rect():
+    t=turtle.Turtle()
+    t.speed(3)
+    
+    t.forward(150)
+    t.right(90) 
+    t.forward(80)
+    t.right(90)
+    t.forward(150)
+    t.right(90)
+    t.forward(80)
+    t.right(90)
+    
+    turtle.done()
+
+draw_rect()
