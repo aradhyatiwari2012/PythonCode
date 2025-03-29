b6ad908ef2b9f3fa09af46f6a23a1d92eb9fb5b4
aradhyatiwari2012 <babali.ramesh@gmail.com>
2025-03-29 12:50:04 +0530

    new file

--- Linear Eq, Shapes/Turtle_draw_gons.py
+++ Linear Eq, Shapes/Turtle_draw_gons.py
@@ -1,21 +1,21 @@
 import turtle  
 
-def int_ang(n):
-    var_1 = (n - 2) * 180 // n
+def ext_ang(n):
+    var_1 = 360//n
     return var_1
 
 def draw_gon(sides, units, angles):
     t = turtle.Turtle()
     t.speed(3)
 
-    for _ in range(sides):   # Use 'sides' here, not 'side'
+    for _ in range(sides):  
         t.forward(units)
         t.right(angles)
 
     turtle.done()
 
-side = int(input("Enter the number of sides: "))  # Corrected input message
-ang = int_ang(side)
+side = int(input("Enter the number of sides: "))  
+ang = ext_ang(side)
 units = int(input("Enter the number of units by which you want to draw the line: "))
 
 draw_gon(side, units, ang)
---
+++ program 16-23/Cube_mensuration.py
@@ -0,0 +1,8 @@
+num_1=int(input("Enter the edge of cube to find the Curved and Total Surface area and Volume:"))
+
+LSA=4 * num_1**2
+TSA=6 * num_1**2
+VOL=num_1**3
+print("The lateral surface area is:",LSA,"square units")
+print("The total surface area is:",TSA,"square units")
+print("The volume is:",VOL,"cubic units")
