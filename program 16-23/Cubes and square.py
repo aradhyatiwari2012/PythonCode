61999e27479757c10d48b0e1f7da989172328ddc
aradhyatiwari2012 <babali.ramesh@gmail.com>
2025-04-04 20:38:51 +0530

    New File added

---
+++ program 16-23/Cubes and square.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter a number:"))
+n1=num1**2
+n2=round(num1**0.5,3)
+n3=num1**3
+n4=round(num1**0.3)
+print("The square root is:",n2)
+print("The square is:",n1)
+print("The cubic root is:",n3)
+print("The cube is:",n4)
